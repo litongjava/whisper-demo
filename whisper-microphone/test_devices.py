784087287cbe62ee93a74c0f944ce90e7804217c
litongjava <litongjava@qq.com>
2023-09-18 23:27:31 -1000

    add query_devices.py

--- whisper-microphone/test_devices.py
+++ whisper-microphone/test_devices.py
@@ -6,7 +6,9 @@ duration = 5  # seconds
 sample_rate = 44100  # Hz
 
 print("Recording for {} seconds...".format(duration))
-audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2, dtype=np.float32)
+audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype=np.float32)
+#audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=2, device=1, dtype=np.float32)
+
 sd.wait()
 
 print("Playing back the recorded audio...")
